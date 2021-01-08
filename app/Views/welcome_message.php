a68fb054cf85ab6ebaf3b4fc9aee984a47dfd0c7
William <carloseduardoramosmedina@gmail.com>
2021-01-08 18:18:20 -0500

    Modifico welcome

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -219,7 +219,7 @@
 
 	<div class="heroe">
 
-		<h1>Hi..! <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+		<h1>Prueba v1! <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
 		<h2>The small framework with powerful features</h2>
 
